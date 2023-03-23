32cabe8c409fc5484f67563e74567afe92c86842
Denis Revin <denis.revin@gmail.com>
2023-03-24 01:49:27 +0400

    Initial Commit

---
+++ StockOverviewIos/StockOverviewIosApp.swift
@@ -0,0 +1,17 @@
+//
+//  StockOverviewIosApp.swift
+//  StockOverviewIos
+//
+//  Created by Ревин Денис on 24.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct StockOverviewIosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
